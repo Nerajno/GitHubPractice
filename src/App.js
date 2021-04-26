177c86f7424c38a2c8dc1ecdb2261d819c8d619c
nerajno <nerajno@gmail.com>
2021-04-26 14:52:02 -0400

    need to implement default value

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import { Navbar, Nav, Button } from 'react-bootstrap';
+
+import { Navbar, Nav } from 'react-bootstrap';
 import Footer from "./Components/Footer";
+import Card from "./Components/Card";
 
 function App(){
-  const API = process.env.REACT_APP_SEARCH_API
+  const API = process.env.REACT_APP_SEARCH_API;
   const [users, setUsers] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
 
   const getUsers = (input) => {
       fetch(input)
       .then(response => response.json())
-      .then((data) => {console.log(data);
-      setUsers(data.results)})
+      .then((data) => {setUsers(data.results)})
   }
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
     if (searchTerm) {
-      getUsers(process.env.REACT_APP_SEARCH_API + searchTerm);
+      getUsers(API + searchTerm);
       setSearchTerm("");
     }
   };
@@ -26,6 +27,8 @@ function App(){
   const handleOnChange = (e) => {
     setSearchTerm(e.target.value);
   };
+
+  console.log(users);
 
   return (
     <div className="App">
@@ -49,7 +52,8 @@ function App(){
         </Navbar.Collapse>
       </Navbar>
       <div className="resultContainer">
-
+       {users.length > 0 &&
+          users.map((user) => <Card key={user.id} {...user} />)}
       </div>
       <Footer />
        </div>
