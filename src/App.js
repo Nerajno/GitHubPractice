cccb3c45ff15cbc3d42fbf92b6167d31f6c198cb
nerajno <nerajno@gmail.com>
2021-04-26 09:33:40 -0400

    implemented basic structure  and logic

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Navbar, Nav, Button } from 'react-bootstrap';
+import Footer from "./Components/Footer";
 
-import Footer from './Components/Footer'
+function App(){
+  const API = process.env.REACT_APP_SEARCH_API
+  const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-function App() {
+  const getUsers = (input) => {
+      fetch(input)
+      .then(response => response.json())
+      .then((data) => {console.log(data);
+      setUsers(data.results)})
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm) {
+      getUsers(process.env.REACT_APP_SEARCH_API + searchTerm);
+      setSearchTerm("");
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-          Learn React
-      </header>
-      <div className="main-container">
-        
+       <div className="main-container">
+       <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Github Search</Navbar.Brand> 
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Docs</Nav.Link>
+          </Nav>
+          <form onSubmit={handleOnSubmit}>
+          <input
+            className="search"
+            type="search"
+            value={searchTerm}
+            onChange={handleOnChange}
+            placeholder="Search...."
+          />
+          </form>
+        </Navbar.Collapse>
+      </Navbar>
+      <div className="resultContainer">
+
       </div>
-      <Footer/>
+      <Footer />
+       </div>
     </div>
   );
 }
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Footer = () => (
     <div className="footer">
+        <p>View Docs on Github.</p>
         <p>If you are reading this ... <a href="https://twitter.com/nerajno" target="_blank" rel="noopener noreferrer">
             Send feedback or follow me on Twitter.</a></p>
     </div>
