e038dd6d733db6092e7da794158e31e72fc04615
nerajno <nerajno@gmail.com>
2021-04-29 23:06:00 -0400

    parts of this was made correctly but structured incorrectly, paused, missed the deadline and needs refactoring

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,36 +1,34 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 
-const UserCard = props => {
-    const userInfo = props.url;
-    
-  const getUserInfo = (userInfo) => {
-        fetch(userInfo)
-        .then(response => response.json())
-        .then((data) => {
-          console.log(data);
-        })
+function UserCard(){
+
+
+
+
+  componentDidMount() { // need a full refactoring
+    const apiUrl = props.url;
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   }
-
-
-
-  return (
-    <Card style={{ width: '18rem' }}>
-  {/* <Card.Img variant="top" src="" /> */}
-  <Card.Body>
-    {/* <Card.Title>Name:{login}</Card.Title> */}
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Card.Text>Followers{}</Card.Text>
-    <Card.Text>Following{}</Card.Text>
-      {/* <Card.Link href="#">{url.value}</Card.Link> */}
-    <Card.Link href="#">Another Link</Card.Link>
-  </Card.Body>
-</Card>
-  );
-};
-
-
+  render() {
+    return (
+        <Card style={{ width: '18rem' }}>
+      {/* <Card.Img variant="top" src="" /> */}
+      <Card.Body>
+        {/* <Card.Title>Name:{login}</Card.Title> */}
+        <Card.Text>
+          Some quick example text to build on the card title and make up the bulk of
+          the card's content.
+        </Card.Text>
+        <Card.Text>Followers{}</Card.Text>
+        <Card.Text>Following{}</Card.Text>
+          {/* <Card.Link href="#">{url.value}</Card.Link> */}
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+      );
+  }
+}
 export default UserCard;
