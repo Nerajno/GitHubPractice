0357f30a1cba458f3ba967c9732a725943b9e812
nerajno <nerajno@gmail.com>
2021-06-03 07:07:30 -0400

    adding notes to catch up with main branch

--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -1,11 +1,14 @@
 import React from 'react';
 // import Pagination from 'react-bootstrap/Pagination';
 
+//firstPage lastPage
+
 const NavButtons = (props) => {
 let navList = [];
 for( let i = 0; (i * props.usersPerPage) < props.totalUsersFound; i++){
   const page = i+1;
-       navList.push(<li class="page-item"><button onClick={() => {props.updatePage(page)}}>{page}</button></li>);
+       navList.push(<li className="page-item"><button onClick={() => {props.updatePage(page)}}>{page}</button></li>);
+      //  key=props.id to fix please
      }
 return navList;
 }
